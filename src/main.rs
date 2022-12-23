a3aae1f524ec0c6ed3705cbd17e357e30d83a2f5
Cade Gillem <cpgillem@gmail.com>
2022-12-22 22:10:35 -0500

    Add connection pool.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@ use std::env;
 use actix_web::{HttpServer, App, web, Responder, HttpResponse, get};
 use diesel::{PgConnection, Connection};
 use dotenvy::dotenv;
+use diesel::r2d2::{self, ConnectionManager};
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 struct AppState {
-    database_connection: PgConnection,
+    db_pool: Pool,
 }
 
-pub fn create_connection() -> PgConnection {
+pub fn create_connection() -> Pool {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set.");
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Could not connect to {}", database_url))
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    r2d2::Pool::builder()
+        .build(manager)
+        .expect("Could not create DB connection pool.")
+    // PgConnection::establish(&database_url)
+    //     .unwrap_or_else(|_| panic!("Could not connect to {}", database_url))
 }
 
 #[get("/")]
@@ -26,7 +33,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(AppState {
-                database_connection: create_connection(),
+                db_pool: create_connection(),
             }))
             .service(index)
     })
